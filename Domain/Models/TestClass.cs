31555f7a947eb0d71e666db90385e0e4e5b97c0d
bini teshome <se.biniyam.teshome@gmail.com>
2020-02-13 20:38:28 +0300

    test class added

---
+++ Domain/Models/TestClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SCMS.API.Domain.Models
+{
+    public class TestClass
+    {
+    }
+}
