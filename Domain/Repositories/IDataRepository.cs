26f9b3ef2760bd130783c3913729e344f8dc70bf
bini teshome <se.biniyam.teshome@gmail.com>
2020-02-20 10:03:48 +0300

    generic repository data access class and interface

---
+++ Domain/Repositories/IDataRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SCMS.API.Domain.Repositories
+{
+    interface IDataRepository<T>
+    {
+        void Delete(T entity);
+        void Delete(object id);
+        IEnumerable<T> Get(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "");
+        T GetById(object id);
+        IEnumerable<T> GetWithRawSql(string query, params object[] parameters);
+        int Insert(T entity);
+        bool Update(T entity);
+        IEnumerable<T> GetAll();
+    }
+}
